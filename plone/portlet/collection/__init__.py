ff392b70da7e41f6ae0aada749952f21f7f37294
Wichert Akkerman <wichert@wiggy.net>
2007-11-19 10:07:35 +0000

    There is no need for an empty initialize
    svn path=/plone.portlet.collection/trunk/; revision=18053

--- plone/portlet/collection/__init__.py
+++ plone/portlet/collection/__init__.py
@@ -1,5 +1,3 @@
 from zope.i18nmessageid import MessageFactory
 CollectionMessageFactory = MessageFactory('plone.portlet.collection')
 
-def initialize(context):
-    """Intializer called when used as a Zope 2 product."""
