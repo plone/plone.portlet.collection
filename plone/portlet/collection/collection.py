b94b81175958483e2559ba9cf8b8717743c3f2f6
David Glick <davidglick@groundwire.org>
2008-06-14 02:32:23 +0000

    added 'Select random items' option.  This LazyMap stuff to avoid instantiating unneeded catalog brains is probably either overkill or should be pushed up the stack...
    svn path=/plone.portlet.collection/trunk/; revision=21180

--- plone/portlet/collection/collection.py
+++ plone/portlet/collection/collection.py
@@ -1,3 +1,5 @@
+import random
+
 from zope.interface import implements
 from zope.component import getMultiAdapter
 
@@ -38,6 +40,12 @@ class ICollectionPortlet(IPortletDataProvider):
                                        "Leave this blank to show all items."),
                        required=False)
                        
+    random = schema.Bool(title=_(u"Select random items"),
+                         description=_(u"If enabled, items will be selected randomly from the collection, "
+                                        "rather than based on its sort order."),
+                         required=True,
+                         default=False)
+                       
     show_more = schema.Bool(title=_(u"Show more... link"),
                        description=_(u"If enabled, a more... link will appear in the footer of the portlet, "
                                       "linking to the underlying Collection."),
@@ -61,13 +69,15 @@ class Assignment(base.Assignment):
     header = u""
     target_collection=None
     limit = None
+    random = False
     show_more = True
     show_dates = False
 
-    def __init__(self, header=u"", target_collection=None, limit=None, show_more=True, show_dates=False):
+    def __init__(self, header=u"", target_collection=None, limit=None, random=False, show_more=True, show_dates=False):
         self.header = header
         self.target_collection = target_collection
         self.limit = limit
+        self.random = random
         self.show_more = show_more
         self.show_dates = show_dates
 
@@ -113,16 +123,54 @@ class Renderer(base.Renderer):
         else:
             return collection.absolute_url()
 
-    @memoize
     def results(self):
-        """ get the actual result brains from the collection.
-            render_cachekey method calls self._data to compute cache key. """
+        """ Get the actual result brains from the collection. 
+            This is a wrapper so that we can memoize if and only if we aren't
+            selecting random items."""
+        if self.data.random:
+            return self._random_results()
+        else:
+            return self._standard_results()
+
+    @memoize
+    def _standard_results(self):
         results = []
         collection = self.collection()
         if collection is not None:
             results = collection.queryCatalog()
             if self.data.limit and self.data.limit > 0:
                 results = results[:self.data.limit]
+        return results
+        
+    # intentionally non-memoized
+    def _random_results(self):
+        results = []
+        collection = self.collection()
+        if collection is not None:
+            """
+            Kids, do not try this at home.
+            
+            We're poking at the internals of the (lazy) catalog results to avoid
+            instantiating catalog brains unnecessarily.
+            
+            We're expecting a LazyCat wrapping two LazyMaps as the return value from
+            Products.ATContentTypes.content.topic.ATTopic.queryCatalog.  The second
+            of these contains the results of the catalog query.  We force sorting
+            off because it's unnecessary and might result in a different structure of
+            lazy objects.
+            
+            Using the correct LazyMap (results._seq[1]), we randomly pick a catalog index
+            and then retrieve it as a catalog brain using the _func method.
+            """
+            
+            results = collection.queryCatalog(sort_on=None)
+            limit = self.data.limit and min(len(results), self.data.limit) or 1
+            try:
+                results = [results._seq[1]._func(i) for i in random.sample(results._seq[1]._seq, limit)]
+            except AttributeError, IndexError:
+                # This handles the cases where the lazy objects returned by the catalog
+                # are structured differently than expected.
+                results = []
         return results
         
     @memoize
--- plone/portlet/collection/tests/test_portlet_collection.py
+++ plone/portlet/collection/tests/test_portlet_collection.py
@@ -114,7 +114,7 @@ class TestCollectionQuery(TestCase):
         return getMultiAdapter((context, request, view, manager, assignment), IPortletRenderer)
 
     def testSimpleQuery(self):
-        # set up our collection to searh for Folders
+        # set up our collection to search for Folders
         crit = self.folder.collection.addCriterion('portal_type', 'ATSimpleStringCriterion')
         crit.setValue('Folder')
 
@@ -135,7 +135,63 @@ class TestCollectionQuery(TestCase):
 
         # we want the portlet to return us the same results as the collection
         self.assertEquals(collection_num_items, len(collectionrenderer.results()))
+        
+    def testRandomQuery(self):
+        # we're being perhaps a bit too clever in random mode with the internals of the
+        # LazyMap returned by the collection query, so let's try a bunch of scenarios
+        # to make sure they work
+        
+        # set up our portlet renderer
+        mapping = PortletAssignmentMapping()
+        request = self.folder.REQUEST
+        mapping['foo'] = collection.Assignment(header=u"title",
+                                               random=True,
+                                               target_collection='/Members/test_user_1_/collection')
+        collectionrenderer = self.renderer(context=None, request=None, view=None, manager=None, assignment=mapping['foo'])
 
+        # add some folders
+        for i in range(6):
+            self.folder.invokeFactory('Folder', 'folder_%s'%i)
+            getattr(self.folder, 'folder_%s'%i).reindexObject()
+
+        # collection with no criteria -- should return empty list, without error
+        self.assertEqual(len(collectionrenderer.results()), 0)
+
+        # let's make sure the results aren't being memoized
+        old_func = self.folder.collection.queryCatalog
+        global collection_was_called
+        collection_was_called = False
+        def mark_collection_called(**kw):
+            global collection_was_called
+            collection_was_called = True
+        self.folder.collection.queryCatalog = mark_collection_called
+        collectionrenderer.results()
+        self.folder.collection.queryCatalog = old_func
+        self.failUnless(collection_was_called)
+        
+        # collection with simple criterion -- should return 1 (random) folder
+        crit = self.folder.collection.addCriterion('portal_type', 'ATSimpleStringCriterion')
+        crit.setValue('Folder')
+        self.assertEqual(len(collectionrenderer.results()), 1)
+        
+        # collection with multiple criteria -- should behave similarly
+        crit = self.folder.collection.addCriterion('Creator', 'ATSimpleStringCriterion')
+        crit.setValue('test_user_1_')
+        collectionrenderer.results()
+        
+        # collection with sorting -- should behave similarly (sort is ignored internally)
+        self.folder.collection.setSortCriterion('modified', False)
+        self.assertEqual(len(collectionrenderer.results()), 1)
+        
+        # same criteria, now with limit set to 2 -- should return 2 (random) folders
+        collectionrenderer.data.limit = 2
+        self.assertEqual(len(collectionrenderer.results()), 2)
+        
+        # make sure there's no error if the limit is greater than the # of results found
+        collectionrenderer.data.limit = 10
+        self.failUnless(len(collectionrenderer.results()) >= 6)
+        
+        
 def test_suite():
     from unittest import TestSuite, makeSuite
     suite = TestSuite()
