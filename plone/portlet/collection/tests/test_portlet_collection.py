1ee3d35a349a0fd0d67544cb9b01a2e90db7a806
Hanno Schlichting <hanno@hannosch.eu>
2011-05-18 15:20:49 +0000

    Whitespace and some pep8
    svn path=/plone.portlet.collection/trunk/; revision=49761

--- plone/portlet/collection/tests/test_portlet_collection.py
+++ plone/portlet/collection/tests/test_portlet_collection.py
@@ -1,26 +1,20 @@
-from zope.component import getUtility, getMultiAdapter
-
+from plone.app.portlets.storage import PortletAssignmentMapping
 from plone.portlets.interfaces import IPortletType
 from plone.portlets.interfaces import IPortletManager
 from plone.portlets.interfaces import IPortletAssignment
 from plone.portlets.interfaces import IPortletDataProvider
 from plone.portlets.interfaces import IPortletRenderer
-
-from plone.app.portlets.storage import PortletAssignmentMapping
+from Products.CMFCore.utils import getToolByName
+from zope.component import getUtility, getMultiAdapter
 
 from plone.portlet.collection import collection
-
 from plone.portlet.collection.tests.base import TestCase
-
-
-from Products.CMFCore.utils import getToolByName
-
 
 
 class TestPortlet(TestCase):
 
     def afterSetUp(self):
-        self.setRoles(('Manager',))
+        self.setRoles(('Manager', ))
 
     def testPortletTypeRegistered(self):
         portlet = getUtility(IPortletType, name='plone.portlet.collection.Collection')
@@ -37,16 +31,13 @@ class TestPortlet(TestCase):
         for m in mapping.keys():
             del mapping[m]
         addview = mapping.restrictedTraverse('+/' + portlet.addview)
-
-        addview.createAndAdd(data={'header' : u"test title"})
-
+        addview.createAndAdd(data={'header': u"test title"})
         self.assertEquals(len(mapping), 1)
         self.failUnless(isinstance(mapping.values()[0], collection.Assignment))
 
     def testInvokeEditView(self):
         mapping = PortletAssignmentMapping()
         request = self.folder.REQUEST
-
         mapping['foo'] = collection.Assignment(header=u"title")
         editview = getMultiAdapter((mapping['foo'], request), name='edit')
         self.failUnless(isinstance(editview, collection.EditForm))
@@ -61,10 +52,11 @@ class TestPortlet(TestCase):
         renderer = getMultiAdapter((context, request, view, manager, assignment), IPortletRenderer)
         self.failUnless(isinstance(renderer, collection.Renderer))
 
+
 class TestRenderer(TestCase):
 
     def afterSetUp(self):
-        self.setRoles(('Manager',))
+        self.setRoles(('Manager', ))
 
     def renderer(self, context=None, request=None, view=None, manager=None, assignment=None):
         context = context or self.folder
@@ -83,26 +75,23 @@ class TestRenderer(TestCase):
         self.assertTrue('title' in output)
 
     def test_collection_path_unicode(self):
-        """
-        Cover problem in #9184
-        """
+        # Cover problem in #9184
         r = self.renderer(context=self.portal,
                           assignment=collection.Assignment(header=u"title",
                                                            target_collection=u"/events"))
-        r  = r.__of__(self.folder)
+        r = r.__of__(self.folder)
         self.assertEqual(r.collection().id, 'events')
-        
+
     def test_css_class(self):
         r = self.renderer(context=self.portal,
                           assignment=collection.Assignment(header=u"Welcome text"))
         self.assertEquals('portlet-collection-welcome-text', r.css_class())
-     
+
 
 class TestCollectionQuery(TestCase):
 
     def afterSetUp(self):
-        self.setRoles(('Manager',))
-        #make a collection
+        self.setRoles(('Manager', ))
         self.collection = self._createType(self.folder, 'Topic', 'collection')
 
     def _createType(self, context, portal_type, id, **kwargs):
@@ -141,7 +130,6 @@ class TestCollectionQuery(TestCase):
         self.failUnless(collection_num_items >= 6)
 
         mapping = PortletAssignmentMapping()
-        request = self.folder.REQUEST
         mapping['foo'] = collection.Assignment(header=u"title", target_collection='/Members/test_user_1_/collection')
         collectionrenderer = self.renderer(context=None, request=None, view=None, manager=None, assignment=mapping['foo'])
 
@@ -155,7 +143,6 @@ class TestCollectionQuery(TestCase):
 
         # set up our portlet renderer
         mapping = PortletAssignmentMapping()
-        request = self.folder.REQUEST
         mapping['foo'] = collection.Assignment(header=u"title",
                                                random=True,
                                                target_collection='/Members/test_user_1_/collection')
@@ -173,6 +160,7 @@ class TestCollectionQuery(TestCase):
         old_func = self.folder.collection.queryCatalog
         global collection_was_called
         collection_was_called = False
+
         def mark_collection_called(**kw):
             global collection_was_called
             collection_was_called = True
