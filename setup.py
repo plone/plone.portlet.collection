0dac53d5ecea954a8e62967b2af8456bdc33cb73
Peter Holzer <peter.holzer@agitator.com>
2017-10-19 00:45:18 +0200

    update classifiers

--- setup.py
+++ setup.py
@@ -10,7 +10,6 @@ setup(name='plone.portlet.collection',
       classifiers=[
           "Environment :: Web Environment",
           "Framework :: Plone",
-          "Framework :: Plone :: 5.0",
           "Framework :: Plone :: 5.1",
           "Framework :: Zope2",
           "License :: OSI Approved :: GNU General Public License (GPL)",
